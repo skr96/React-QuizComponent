fa2adfde7fcf04caf413b56599bf36db96f13b36
Sudhanshu Kumar <skrch96@gmail.com>
2018-06-29 15:24:39 +0530

    Module 1 completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
+import  Quiz from './quiz.js'
+
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <Quiz/>
     )
   }
 }
