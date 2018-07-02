f43bbf9d43e25df9619836892d96c08262eb3483
Sudhanshu Kumar <skrch96@gmail.com>
2018-07-02 14:55:48 +0530

    Replaced React.Component with Component to pass tests

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json')
 
-class Quiz extends React.Component{
+class Quiz extends Component {
     constructor(props){
         super(props);
         this.state = {
@@ -12,7 +12,7 @@ class Quiz extends React.Component{
     render(){
         return (
             <div> 
-                <div classname = "QuizQuestion"> 
+                <div className = "QuizQuestion"> 
                     {quizData.quiz_questions[0].instruction_text} 
                 </div> 
             </div>
